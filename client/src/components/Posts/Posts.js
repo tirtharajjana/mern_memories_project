fcfce054931258c2597a2a6f009c52ee471cd3a6
Tirtharaj <tirtharajjana615@gmail.com>
2021-08-19 22:00:51 +0530

    styles

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Post from './Post/Post';
+import useStyles from './styles';
 const Posts = () => {
+    const classes = useStyles();
     return (
         <>
-            Posts
+            <h1 className={classes.something}>POSTS</h1>
             <Post/>
             <Post/>
         </>
