8a9c1ad11e69f07fc84fa7654562c3334a5147fe
Tirtharaj <tirtharajjana615@gmail.com>
2021-08-24 22:13:12 +0530

    debuging

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,17 +1,6 @@
-
-// eslint-disable-next-line import/no-anonymous-default-export
-// export default (posts = [], action) => {
-//     switch (action.type) {
-//         case 'FETCH_ALL':
-//             return action.payload;
-//         case 'CREATE':
-//             return [...posts, action.payload];
-//         default:
-//             return posts;
-//     }
-// }
 import { FETCH_ALL, CREATE, UPDATE, DELETE, LIKE } from '../constants/actionTypes';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (posts = [], action) => {
   switch (action.type) {
     case FETCH_ALL:
--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
-import postsRoutes from './routes/posts.js';
+import postRoutes from './routes/posts.js';
 
 const app = express();
 
-
-
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
-app.use('/posts', postsRoutes);
-const CONNECTION_URL = "mongodb://localhost:27017/test";
-const PORT = process.env.PORT || 5000;
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb://localhost:27017/test';
+const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => { console.log(`Server running on port: ${PORT}`) }))
-    .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
