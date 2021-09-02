fd6e072e333be006587ffa6eb8a04699093412ab
Tirtharaj <tirtharajjana615@gmail.com>
2021-09-02 21:46:38 +0530

    Style frontend

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -32,8 +32,8 @@ const Home = () => {
 
     const searchPost = () => {
         if (search.trim() || tags) {
-            dispatch(getPostsBySearch({ search, tags: tags.join(',') }))
-
+            dispatch(getPostsBySearch({ search, tags: tags.join(',') }));
+            history.push(`/posts/search?searchQuery=${search || 'none'}&tags=${tags.join(',')}`)
         } else {
             history.push('/')
         }
